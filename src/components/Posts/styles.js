ba771173110fbd1e7b2cd7dc1f3fce5c55b30f95
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-20 15:50:58 +0300

    Styling Post Component

---
+++ src/components/Posts/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+    mainContainer : {
+        display : 'flex',
+        alignContent : 'center'
+    },
+    smMargin : {
+        margin: theme.spacing(1)
+    },
+    actionDiv : {
+        textAlign : 'center'
+    }
+}))
