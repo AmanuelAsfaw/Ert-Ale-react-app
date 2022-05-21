294a37cb27813de686368c6368194e221f2a537c
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-21 11:28:43 +0300

    Dispatch getPosts action while component mounted

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import Form from "./components/Form/Form";
-import Posts from "./components/Posts/Posts";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import Form from "./components/Form/Form"
+import Posts from "./components/Posts/Posts"
+import { getPosts } from './actions/posts.js'
 import ert_ale from "./images/images.jpg"
 import useStyles from './styles'
 
 function App() {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  },[dispatch])
 
   return (
    <Container maxWidth="lg">
      <AppBar className={classes.appBar} position="static" color="inherit">
        <Typography className={classes.heading} variant="h2" align="center">Ert-Ale</Typography>
-       <img className={classes.image} src={ert_ale} alt="memories" height="60"/>
+       <img className={classes.image} src={ert_ale} alt="memories" height="60" style={ {'borderRadius' : '100px', 'marginTop': '5px'}}/>
      </AppBar>
      <Grow in>
        <Container>
